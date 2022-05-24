93004eeb981e48cdcdae747d6bdf7163ea0d640a
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-05-24 09:06:40 +0200

    Eslint install

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-new": 0,
+        "array-callback-return": 0,
+        quotes: ["error", "double"]
+    }
+}
